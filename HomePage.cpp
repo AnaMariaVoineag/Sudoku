a46e0b290ff2972f7c456d1fb7629b1f2979f487
Meowzer Meow <loretta@0qls7.onmicrosoft.com>
2023-10-22 19:13:49 +0300

    I added a grd as background.

--- HomePage.cpp
+++ HomePage.cpp
@@ -1,8 +1,20 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(1920, 1080), "Sudoku");
+
+    // Load the background image
+    sf::Texture backgroundTexture;
+    if (!backgroundTexture.loadFromFile("images/grid.jpg"))
+    {
+        std::cout << "Could not find the background image" << std::endl;
+        return 1; // Return an error code
+    }
+
+    // Create a sprite for the background
+    sf::Sprite backgroundSprite(backgroundTexture);
 
     while (window.isOpen())
     {
@@ -15,14 +27,19 @@ int main()
         }
 
         // Convert the hex color to RGB
-        sf::Color backgroundColor(29, 34, 56);
         sf::Color menuColor(38, 45, 71);
 
-        //sf::RectangleShape rectangle(sf::Vector2f(308.f, 1080.f));
+        sf::RectangleShape rectangle(sf::Vector2f(308.f, 1080.f));
         rectangle.setFillColor(menuColor);
-        window.clear(backgroundColor);
 
+        window.clear();
+
+        // Draw the background image first
+        window.draw(backgroundSprite);
+
+        // Draw the menu rectangle
         window.draw(rectangle);
+
         window.display();
     }
 
