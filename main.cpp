19a7d82a6a221d6f8410f667d2eb4db7447b12d9
Katrianne <katia.h.hann@gmail.com>
2023-11-13 16:49:07 +0200

    Added sudoku generator, tested sudoku generator, all seems to be working accordingly.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include "Sudoku.h"
+#include "SudokuGenerator.h"
 
 int main()
-{
+{   
+	srand(static_cast<unsigned>(time(0)));
+	SudokuGenerator* sdkgen = new SudokuGenerator();
+	sdkgen->alegere();
+	sdkgen->umplu();
+	sdkgen->afis_matr();
+	std::cout << '\n'<<'\n';
+	sdkgen->afis_matrsol();
+	sdkgen->~SudokuGenerator();
 	Sudoku sudoku;
 
 	sudoku.run();
